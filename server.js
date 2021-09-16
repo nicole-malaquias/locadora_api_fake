3c811a8104b4fcb07077d388a61fcd802190b4e5
Nicole <nicolemalaquias91@gmail.com>
2021-09-16 18:42:32 -0300

    inicitial commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const cors = require("cors");
+const jsonServer = require("json-server");
+const auth = require("json-server-auth");
+
+const app = jsonServer.create();
+const router = jsonServer.router("db.json");
+const port = process.env.PORT || 3001;
+
+app.db = router.db;
+
+const rules = auth.rewriter({
+  "/movies*": "/664/movies$1",
+});
+
+app.use(cors());
+app.use(rules);
+app.use(auth);
+app.use(router);
+app.listen(port);
+
+console.log("Server is running on port:", port);
